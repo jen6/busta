26b3efe80ee6d24144a1a2d91c05508daeab4016
jen6 <fjen6b@gmail.com>
2015-06-13 17:38:00 +0900

    error edit

--- src/util.go
+++ src/util.go
@@ -6,24 +6,23 @@ import (
 	"log"
 )
 
-func init(){
+func init() {
 	dbmap = make_dbmap()
 
 }
 
-func hasher(str string) string{
+func hasher(str string) string {
 	it := sha256.New()
 	hash_arr := []byte(str)
-	copy(hash_arr[:],str)
+	copy(hash_arr[:], str)
 	it.Write(hash_arr)
 	md := it.Sum(nil)
 	mdStr := hex.EncodeToString(md)
 	return mdStr
 }
 
-
-func check_err(err error, msg string){
-	if err != nil{
-		log.Fatalln(msg, err)
+func check_err(err error, msg string) {
+	if err != nil {
+		log.Println(msg, err)
 	}
 }
