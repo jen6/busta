f127bc817c731207b1b9fded13a6802e0fa05860
jen6 <fjen6b@gmail.com>
2015-06-15 09:24:16 +0900

    change main.go make user/login simple  and erase insert user in main

--- src/main.go
+++ src/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/go-martini/martini"
 	_ "github.com/go-sql-driver/mysql"
@@ -16,29 +14,9 @@ func main() {
 		return "hello"
 	})
 
-	//nw := newUser("jen6", "abcd", "손건")
-	//	err := dbmap.Insert(&nw)
-	nb := newBus("jen6", "hello", "helloworld")
-	err := dbmap.Insert(&nb)
-	check_err(err, "Insert failed")
-
-	//	m.Post("/user/login", func() string {
-	//		return "use form!"
-	//	})
 	m.Post("/user/login", binding.Bind(user_bind{}), func(lg user_bind) string {
-		log.Println("login handler")
-		user := selectUser(lg.UserId)
-		log.Printf("%s %s", lg.UserId, lg.UserPw)
-		log.Printf("%s %s", user.UserId, user.UserPw)
-		if user.UserId == "" {
-			return "no User"
-		}
-		hash_pw := hasher(lg.UserPw)
-		if hash_pw == user.UserPw {
-			return "Good!"
-		} else {
-			return "Fuck!"
-		}
+		return login(lg)
 	})
+
 	m.RunOnAddr(":8989")
 }
